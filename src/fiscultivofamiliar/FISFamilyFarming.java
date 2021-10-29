05e1aa01877c5a491f6b3d08b12b0f71c0e80d44
Nicolas Cardona <ncardonab@unal.edu.co>
2021-10-29 16:13:31 -0500

    Estructura de archivos fcl y java listo

---
+++ src/fiscultivofamiliar/FISFamilyFarming.java
@@ -0,0 +1,14 @@
+package fiscultivofamiliar;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nclsc
+ */
+public class FISFamilyFarming {
+    
+}
